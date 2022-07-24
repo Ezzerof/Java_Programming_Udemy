8a3c61e3080c6cee209cf0d35f7afaf0ab1674c1
Ezzerof <christian.bitca@gmail.com>
2022-07-24 10:57:57 +0100

    BarkingDog/LeapYearCalc

---
+++ src/Barking_Dog/barkingDog.java
@@ -0,0 +1,23 @@
+package Barking_Dog;
+
+public class barkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        boolean result = false;
+
+        if (hourOfDay > 23 || hourOfDay < 0) {
+            result = false;
+        } else {
+            if (barking == true && hourOfDay < 8 || hourOfDay > 22) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(false,2));
+    }
+
+}
---
+++ src/Leap_Year_Calculator/LeapYearCalculator.java
@@ -0,0 +1,35 @@
+package Leap_Year_Calculator;
+
+public class LeapYearCalculator {
+
+    public static boolean isLeapYear(int year){
+
+        boolean result = false;
+
+        if (year<0) {
+            result = false;
+        } else {
+
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0){
+                        result = true;
+                    } else {
+                        result = false;
+                    }
+                } else {
+                    result = true;
+                }
+            } else {
+                result = false;
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2000));
+    }
+
+}
