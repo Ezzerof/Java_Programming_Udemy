79001dff62a035aa1027f6a23335b9a2466ed213
Ezzerof <christian.bitca@gmail.com>
2022-08-08 17:35:53 +0100

    cylinder exercise

---
+++ src/cylinder/Main.java
@@ -0,0 +1,17 @@
+package cylinder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
+
+    }
+}
